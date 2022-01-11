ab4f59e96894fee1f5746229278205ef3333c7a8
kwiatkowskijan <kwiatkojan@gmail.com>
2022-01-11 22:28:49 +0100

    firstCommit

---
+++ script.js
@@ -0,0 +1,21 @@
+function one() {
+    var one = document.getElementById("one").value;
+    var calc = document.getElementById("calc");
+    one = 1;
+
+    calc.innerHTML = one;
+}
+
+function two() {
+    var two = document.getElementById("two").value;
+    var calc = document.getElementById("calc");
+    two = 2;
+    calc.innerHTML = two;
+}
+
+function addition() {
+    var addition = document.getElementById("addition").value;
+    var calc = document.getElementById("calc");
+    addition = "+"
+    calc.innerHTML = addition;
+}
