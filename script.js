c5e4851cf51e17ee75fe0a7c887ef9b67ba6ec67
kwiatkowskijan <kwiatkojan@gmail.com>
2022-01-12 19:29:38 +0100

    first script commit

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-function one() {
-    var one = document.getElementById("one").value;
-    var calc = document.getElementById("calc");
-    one = 1;
+function clear() {
+    var calcElement = document.getElementById("calc");
+    var resultElement = document.getElementById("result");
 
-    calc.innerHTML = one;
+    console.log(calcElement);
+    calcElement.innerHTML = "";
+    resultElement.innerHTML = "";
 }
 
-function two() {
-    var two = document.getElementById("two").value;
-    var calc = document.getElementById("calc");
-    two = 2;
-    calc.innerHTML = two;
-}
-
-function addition() {
-    var addition = document.getElementById("addition").value;
-    var calc = document.getElementById("calc");
-    addition = "+"
-    calc.innerHTML = addition;
+function addNumber(number) {
+    var calcElement = document.getElementById("calc");
+    
+    if (calcElement.innerHTML == undefined) {
+        calcElement.innerHTML = number;
+    } else {
+        calcElement.innerHTML = calcElement.innerHTML + number;
+    }
 }
