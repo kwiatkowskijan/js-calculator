8f3adc8f42139feb4a3b3b9368c189a7930c60fa
kwiatkowskijan <kwiatkojan@gmail.com>
2022-01-12 22:07:19 +0100

    script v1.0

--- script.js
+++ script.js
@@ -1,18 +1,62 @@
-function clear() {
-    var calcElement = document.getElementById("calc");
-    var resultElement = document.getElementById("result");
-
-    console.log(calcElement);
-    calcElement.innerHTML = "";
-    resultElement.innerHTML = "";
+function clearCalculation() {
+    document.getElementById("calc").innerHTML = "";
+    clearResult();
 }
 
-function addNumber(number) {
+function clearResult() {
+    document.getElementById("result").innerHTML = "";
+}
+
+function isOperationCharacter(character) {
+    if (character === '+' || character === '-' || character === '*' || character === '/' || character === '.') {
+        return true;
+    }
+    return false;
+}
+
+function addChar(character) {
     var calcElement = document.getElementById("calc");
-    
+    clearResult();
+
+    //input data validation
+    if (calcElement.innerHTML != undefined && calcElement.innerHTML.length > 0) {
+        var lastChar = calcElement.innerHTML.substring(calcElement.innerHTML.length - 1);
+        if (isOperationCharacter(lastChar) && isOperationCharacter(character)) {
+            backspace();
+        }
+    }
+
+    //add char to calc element
     if (calcElement.innerHTML == undefined) {
-        calcElement.innerHTML = number;
+        calcElement.innerHTML = character;
     } else {
-        calcElement.innerHTML = calcElement.innerHTML + number;
+        calcElement.innerHTML = calcElement.innerHTML + character;
+    }
+}
+
+
+function calculateResult() {
+    var calcValue = document.getElementById("calc").innerHTML;
+
+    if (calcValue != undefined && calcValue.length > 0 && isOperationCharacter(calcValue.substring(calcValue.length - 1))) {
+        backspace();
+        calcValue = document.getElementById("calc").innerHTML;
+    }
+
+    if (calcValue != undefined && calcValue.length > 0) {
+        var result = eval(calcValue);
+        if (result == Number.POSITIVE_INFINITY || Number.isNaN(result) || result == Number.NEGATIVE_INFINITY) {
+            result = "Niedozwolone działanie";
+        }
+        document.getElementById("result").innerHTML = result;
+    }
+}
+
+function backspace() {
+    var calcValue = document.getElementById("calc").innerHTML;
+    clearResult();
+
+    if (calcValue != undefined && calcValue.length > 0) {
+        document.getElementById("calc").innerHTML = calcValue.substring(0, calcValue.length - 1);
     }
 }
